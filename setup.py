7a7b1eeab4526e1e43533fc50520dc1ed9947623
Ron Vergis <ron.vergis@gmail.com>
2025-08-22 09:52:55 -0500

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cncf-serverless-workflow",
-    version="0.1.0",
+    version="0.1.1",
     packages=["cncf_serverless_workflow"],
     install_requires=["pyyaml", "jq", "jsonschema", "pytest"],
     include_package_data=True,
